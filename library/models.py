7cfb886b1ce0089e65d394a94fc3aed8fc608a9c
n4itsric <anticris9303@gmail.com>
2016-09-06 14:42:44 -0500

    add models

--- library/models.py
+++ library/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 
 class Autor(models.Model):
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=255)
 
 
 class Tema(models.Model):
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=255)
 
 
 class Libro(models.Model):
     autor = models.ManyToManyField(Autor)
     tema = models.ForeignKey(Tema)
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=255)
     publicacion = models.DateField()
